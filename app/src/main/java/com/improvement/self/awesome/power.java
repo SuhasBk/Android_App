1b868edd1152ce5d21e4bb2f8825d82b67f1138c
Suhas Bk <kowligi1998@gmail.com>
2018-06-06 22:28:14 +0530

    Android App

---
+++ app/src/main/java/com/improvement/self/awesome/power.java
@@ -0,0 +1,31 @@
+package com.improvement.self.awesome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class power extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power);
+    }
+
+    public void cam(View view){
+        Intent launch = getPackageManager().getLaunchIntentForPackage("com.facebook.katana");
+        startActivity(launch);
+    }
+
+    public void insta(View view){
+        Intent launch = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+        startActivity(launch);
+    }
+
+    public void what(View view) {
+        Intent launch = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+        startActivity(launch);
+    }
+}
