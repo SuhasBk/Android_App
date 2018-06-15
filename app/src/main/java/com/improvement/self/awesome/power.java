93a7489830d79134569be83b5736d37c551af133
Suhas Bk <kowligi1998@gmail.com>
2018-06-15 20:27:45 +0530

    update

--- app/src/main/java/com/improvement/self/awesome/power.java
+++ app/src/main/java/com/improvement/self/awesome/power.java
@@ -1,10 +1,15 @@
 package com.improvement.self.awesome;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
 
 public class power extends AppCompatActivity {
 
@@ -27,5 +32,19 @@ public class power extends AppCompatActivity {
     public void what(View view) {
         Intent launch = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
         startActivity(launch);
+    }
+
+    public void on(View view){
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifi.setWifiEnabled(true);
+    }
+
+    public void off(View view){
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifi.setWifiEnabled(false);
+    }
+
+    public void nextact(View view){
+        startActivity(new Intent(power.this, Login.class));
     }
 }
